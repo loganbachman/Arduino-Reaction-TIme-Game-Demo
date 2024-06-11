de3329733cfa4b1777e8f770b6dbf28379389783
loganbachman <loganbachman04@gmail.com>
2024-06-11 14:33:48 -0700

    ArduinoReactionTimeGame.ino

--- ArduinoReactionTimeGame.ino
+++ ArduinoReactionTimeGame.ino
@@ -1,24 +1,16 @@
 #include <Adafruit_CircuitPlayground.h>
 #include <AsyncDelay.h>
 
-AsyncDelay delay_1s;
 volatile bool switchFlag = false;
-volatile bool buttonRightFlag = false;
 volatile bool buttonLeftFlag = false;
 bool switchState = false;
-bool buttonRightState = false;
 bool buttonLeftState = false;
-const int buttonPinRight = 5;
 const int buttonPinLeft = 4;
 const int switchPin = 7;
-unsigned long timePassed;
-unsigned long startTime;
-unsigned long timeElapsed;
-int randDelay = random(10000);
-volatile bool level1Activated = false;
-volatile bool level2Activated = false;
-volatile bool level3Activated = false;
-
+unsigned long startTime; //variable that starts timer
+unsigned long timeElapsed; //variable that counts time passed
+int randDelay = random(10000); //sets random delay between 1-10 seconds
+int score = 0;
 
 void buttonLeftISR() {
   buttonLeftFlag = true;
@@ -38,33 +30,36 @@ void setup() {
 void loop() {
   if (buttonLeftFlag) {
     delay(5);
-    buttonLeftState = !buttonLeftState;
+    buttonLeftState = !buttonLeftState; //set button state to true
     buttonLeftFlag = false;
   }
 
   if (switchFlag == true) {
     startTime = 0;
+    score = 0;
     delay(randDelay);
     for (int i = 0; i < 10; i++) {
       CircuitPlayground.setPixelColor(i, 0, 255, 255);
     }
     delay(100);
     CircuitPlayground.clearPixels();
-    level1Activated = true;
-    startTime = millis() + 100;
-    Serial.println("Timer Started");
+    startTime = millis();
     switchFlag = false;
   }
 
   if (buttonLeftState == true) {
-    timeElapsed = millis() - startTime;
+    timeElapsed = millis() - startTime; //converts start time to record time passed when button's pressed
+    delay(5);
     Serial.print("Reaction Time: ");
     Serial.print(timeElapsed);
     Serial.println(" ms");
+    Serial.print("Score: ");
+    Serial.println(score);
   }
 
-  if (timeElapsed <= 500 && buttonLeftState == true && level1Activated == true && !level2Activated) {
-    CircuitPlayground.playTone(100, 100);
+  if (timeElapsed <= 250 && buttonLeftState == true && score == 0) {
+    score = 1; //change score as a new level has been reached
+    CircuitPlayground.playTone(100, 200); //play sound to signify you reacted in time
     startTime = 0;
     delay(randDelay);
     for (int i = 0; i < 10; i++) {
@@ -72,12 +67,10 @@ void loop() {
     }
     delay(100);
     CircuitPlayground.clearPixels();
-    level2Activated = true;
-    startTime = millis() + 100;
-  }
-
-  if (timeElapsed <= 250 && buttonLeftState == true && level2Activated == true && !level3Activated) {
-    CircuitPlayground.playTone(100, 100);
+    startTime = millis(); //set starttime
+  } else if (timeElapsed <= 200 && buttonLeftState == true && score == 1) {
+    CircuitPlayground.playTone(100, 200);
+    score = 2;
     startTime = 0;
     delay(randDelay);
     for (int i = 0; i < 10; i++) {
@@ -85,18 +78,10 @@ void loop() {
     }
     delay(100);
     CircuitPlayground.clearPixels();
-    level3Activated = true;
-    startTime = millis() + 100;
-  }
-
-  if (timeElapsed <= 150 && buttonLeftState == true && level3Activated == true) {
-    CircuitPlayground.playTone(100, 100);
-  }
-
-  if (level1Activated && level2Activated && level3Activated) {
-    level1Activated = false;
-    level2Activated = false;
-    level3Activated = false;
-    startTime = 0;
+    startTime = millis();
+  } else if (timeElapsed <= 150 && buttonLeftState == true && score == 2) {
+    CircuitPlayground.playTone(100, 200);
+    score = 3;
+    Serial.print("All Levels Completed!"); //end of game
   }
 }
